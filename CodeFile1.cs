fd99fcceee4c53d27c30147fd16824e8ca3892d3
Kastt2705 <zhenya.litovchenko.2000@mail.ru>
2022-10-29 18:37:42 +0500

    Number

---
+++ CodeFile1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Pohui
+{
+    class Program
+    {
+        static void Main()
+        {
+            var a = int.Parse(Console.ReadLine());
+            var b = int.Parse(Console.ReadLine());
+            var c = int.Parse(Console.ReadLine());
+            for (int i = a; i < b; i++)
+            {
+                if (i % c == 0)
+                    Console.WriteLine(i);
+            }
+        }
+    }
+}
