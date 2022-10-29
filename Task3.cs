d5cc585613bb862a5856dfa4edf46c5f4f43ff94
FazylovV <vladislav.fazylov04@gmail.com>
2022-10-29 18:32:54 +0500

    Really completed

--- Task3.cs
+++ Task3.cs
@@ -4,9 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PractiseLessons
+namespace Pohui
 {
-    internal class Class1
+    public class Task3
     {
+        public int CalcSumOfOdd(int max)
+        {
+            int sum = 0;
+            for (int i = 1; i <= max; i += 2)
+                sum += i;
+            return sum;
+        }
     }
 }
